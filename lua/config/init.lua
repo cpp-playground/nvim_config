7affc723b78de1190caec18889b145f38bed4698
Leo <leo@ascent.ai>
2022-10-07 00:00:58 +0900

    Move meptreeconfig to its own module

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -9,32 +9,8 @@ M.setup = function(config)
     require 'config.alpha'
     require 'config.telescope'
 
-    local language_support_cfg = require("config.language_support")
-    language_support_cfg.setup(config.language_support)
-
-    require("neo-tree").setup({
-        close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
-        popup_border_style = "rounded",
-        default_component_configs = {
-            git_status = {
-
-                symbols = {
-                    -- Change type
-                    added     = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
-                    modified  = "", -- or "", but this is redundant info if you use git_status_colors on the name
-                    deleted   = "", -- this can only be used in the git_status source
-                    renamed   = "", -- this can only be used in the git_status source
-                    -- Status type
-                    untracked = "",
-                    ignored   = "",
-                    unstaged  = "",
-                    staged    = "",
-                    conflict  = "",
-                }
-            },
-        }
-    })
-
+    require("config.language_support").setup(config.language_support)
+    require("config.neo-tree").setup()
 
     require("scrollbar").setup()
 
